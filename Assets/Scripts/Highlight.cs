1ee6385bee55aeb02717ed6b953bfe33feafb16a
Ni Putu Angelina Beatrix Fonda <103482389+beatrixangel123@users.noreply.github.com>
2026-03-12 21:50:32 +0800

    Level 1 on progres (masi bug)
    - pick up item
    - character movement
    - highlight item
    - drop item

--- Assets/Scripts/Highlight.cs
+++ Assets/Scripts/Highlight.cs
@@ -1,16 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Highlight : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private List<Renderer> renderers;
+
+    [SerializeField]
+    private Color color = Color.white;
+
+    private List<Material> materials;
+
+    private void Awake()
     {
-        
+        materials = new List<Material>();
+        foreach (var renderer in renderers)
+        {
+            //a single child-object might have multiple materials on it
+            //that is why we need to all materials with "s"
+            materials.AddRange(new List<Material>(renderer.materials));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ToggleHighlight(bool val)
     {
-        
+        if (val)
+        {
+            foreach (var material in materials)
+            {
+                //we need to enable the emission
+                material.EnableKeyword("_EMISSION");
+                //befoew we can set the color
+                material.SetColor("_EmissionColor", color);
+            }
+        }
+
+        else
+        {
+            foreach (var material in materials)
+            {
+                // we can just disable the emission
+                //if we don't use emission color anywhere else
+                material.DisableKeyword("_EMISSION");
+            }
+        }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -6,19 +8,19 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 5f;
 
-   public Transform maya;
-public float DP = 2.5f;
+    public Transform maya;
+   
+    public float DP = 2.5f;
 
     void Update()
     {
-       float distance = Vector3.Distance(transform.position, maya.position);
+        float distance = Vector3.Distance(transform.position, maya.position);
+        float currentSpeed = speed;
 
-float currentSpeed = speed;
-
-if(distance > DP)
-{
-    currentSpeed = speed * 0.2f;
-}
+        if(distance > DP)
+            {
+                currentSpeed = speed * 0.2f;
+            }
 
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
